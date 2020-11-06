93ca4e21d41a2e4b356d12027cfa6d6a94a07b54
ryusei <perusonaryu1127@gmail.com>
2020-11-06 19:54:34 +0900

    軽く修正

--- public/js/app.js
+++ public/js/app.js
@@ -104626,7 +104626,9 @@ try {
 window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-if (false) {}
+if (true) {
+  window.axios.defaults.baseURL = "http://perusonaryu.sakura.ne.jp/your-ryokan";
+}
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
